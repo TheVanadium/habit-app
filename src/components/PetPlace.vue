f3db14dd03d43170fbc5fd707a72a4349b3f23de
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-01-11 08:48:00 -0800

    refactor: generate pets from arrays
    multiple habit pets were originally hard coded in
    now they're called from array in the data()

--- src/components/PetPlace.vue
+++ src/components/PetPlace.vue
@@ -3,6 +3,16 @@ import HabitPet from './HabitPet.vue'
 
 export default {
     name: "PetPlace",
+    data() {
+        return {
+            pets: [
+                {id: 1, species: 'dog'},
+                {id: 2, species: 'cat'},
+                {id: 3, species: 'bird'},
+                {id: 4, species: 'dog'},
+            ],
+        };
+    },
     components: {
         HabitPet
     }
@@ -10,8 +20,6 @@ export default {
 </script>
 <template>
     <div id="pet-place">
-        <HabitPet species="bird"></HabitPet>
-        <HabitPet species="cat"></HabitPet>
-        <HabitPet species="dog"></HabitPet>
+        <HabitPet v-for="pet in this.pets" :key="pet.id" :species="pet.species"></HabitPet>
     </div>
 </template>
