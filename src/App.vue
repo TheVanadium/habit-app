62acd1e559f01648dcde50fe89d0c1ee3534db5b
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-01-11 08:51:58 -0800

    feat: add non-functional "add pet" button
    functionality will be added later

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
-import PetPlace from './components/PetPlace.vue'
+import PetPlace from './components/PetPlace.vue';
+import AddPetButton from './components/AddPetButton.vue';
 
 export default {
   name: 'App',
@@ -9,7 +10,8 @@ export default {
     }
   },
   components: {
-    PetPlace
+    PetPlace,
+    AddPetButton,
   }
 }
 </script>
@@ -18,14 +20,15 @@ export default {
   <div id="app">
       <p id="header">{{ msg }}!</p>
       <PetPlace></PetPlace>
+      <AddPetButton></AddPetButton>
     <router-view></router-view>
   </div>
 </template>
 
 <style>
 :root {
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
 }
 #header {
   height: 10vh;
@@ -35,9 +38,16 @@ export default {
   align-items: center;
 }
 #pet-place {
-    height: 90vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#add-pet-button {
+  height: 10vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/AddPetButton.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+    name: "AddPetButton",
+}
+</script>
+<template>
+    <button id="add-pet-button">Add Pet</button>
+</template>
