df6f2444fd3bd139573491d55fa69d744a377d0f
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2023-01-24 09:23:10 -0800

    feat: create basic unstyled bowl component

---
+++ src/components/PetBowl.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "PetBowl",
+    props: {
+        id: Number,
+        petName: String,
+    },
+    data: {
+        isFull: true,
+        bowlSrcFull: "/src/assets/bowl_full.jpg",
+        bowlSrcEmpty: "/src/assets/bowl_empty.jpg",
+    },
+    computed: {
+        bowlImageSrc() {
+            return this.isFull ? this.bowlSrcFull : this.bowlSrcEmpty;
+        }
+    },
+}
+</script>
+ <template>
+    <div>
+        <div class="pet-bowl" :id="this.id">
+            <img v-on:click="this.petIsClean = !this.petIsClean" v-on:mouseenter="petMoveRandom" :src="petImageSrc" :alt="petImageAlt">
+        </div>    
+    </div>
+ </template>
+
+ <style>
+    
+ </style>
